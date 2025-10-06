877e30011fc53408643e4da144e73c72e717c4b1
Tiago Figueiredo Reis <tiagoreistp@hotmail.com>
2025-10-06 18:27:39 -0300

    feat: created level scene and added player movements

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    BoxCollider2D boxCollider;
+    private float playerHorDir;
+    [SerializeField] private float velocityX, velocityY;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    
+    void Update()
+    {
+        Running();
+    }
+
+    void Running()
+    {
+        playerRb.linearVelocityX = playerHorDir * velocityX * Time.deltaTime;
+        
+        bool isRunning = Mathf.Abs(playerRb.linearVelocityX) > 0;
+        
+        if(isRunning) Flip();
+    }
+
+    void Flip()
+    {
+        transform.localScale = new Vector2(Mathf.Sign(playerRb.linearVelocityX) * 4, 4);
+    }
+
+    void OnMove(InputValue value)
+    {
+        playerHorDir = value.Get<Vector2>().x;
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed && boxCollider.IsTouchingLayers(LayerMask.GetMask("Foreground")))
+        {
+            playerRb.linearVelocityY = velocityY * Time.deltaTime;
+        }
+    }
+}
